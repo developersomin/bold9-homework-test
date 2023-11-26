6fbf8f57b6b13ed2a8cee57df22c659622b1916b
developersomin <angiwon12345@nate.com>
2023-11-26 20:16:45 +0900

    feat: 게시글 작성 구현
    feat-#7

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,10 @@
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from "@apollo/server/standalone";
 import { userResolver } from './user/user.resolver.js';
-import path from 'path';
 import { userTypeDefs } from './user/user.typeDefs.js';
 import { postTypeDefs } from './post/post.typeDefs.js';
 import { commentTypeDefs } from './comment/comment.typeDefs.js';
-
-/*const typeDefs = `#graphql
-    type User {
-        id: String
-        email: String
-        password: String
-        name: String
-        createdAt: String
-        posts: [Post]
-    }
-    type Post {
-        id: String
-        title: String
-        content: String
-        isPublished: Boolean
-        authorId: String
-        createdAt: String
-        author: User
-        comments: [Comment]
-    }
-    type Comment {
-        id: String
-        content: String
-        postId: String
-        createdAt: String
-        post: Post
-    }
-    type Query {
-        getUsers: User
-    }
-    input CreateUserInput {
-        email: String!
-        password: String!
-        name: String!
-    }
-    type Mutation {
-        createUser(createUserInput: CreateUserInput!):User
-    }
-`*/
+import { postResolver } from './post/post.resolver.js';
 
 const typeDefs =`
     ${userTypeDefs}
@@ -53,6 +14,7 @@ const typeDefs =`
 
 const resolvers = {
     ...userResolver,
+    ...postResolver
 };
 
 const server = new ApolloServer({
